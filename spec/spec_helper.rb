f2bf699129475347bdd26a98fb82d0ee87685e08
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2019-10-23 17:54:21 +0300

    Set example domains

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,6 @@ require 'allure-rspec'
 Allure.configure do |c|
   c.results_directory = 'report/allure-results'
   c.clean_results_directory = true
-  c.link_tms_pattern = 'http://www.jira.com/browse/{}'
-  c.link_issue_pattern = 'http://www.jira.com/browse/{}'
+  c.link_tms_pattern = 'https://example.org/tms/{}'
+  c.link_issue_pattern = 'https://example.org/issue/{}'
 end
