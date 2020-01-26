34c6a90f4746c26eb1dbed48cdb325f0f641694f
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2020-01-26 23:14:55 +0200

    Add hook example in rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,15 @@ Allure.configure do |c|
   c.link_tms_pattern = 'https://example.org/tms/{}'
   c.link_issue_pattern = 'https://example.org/issue/{}'
 end
+
+RSpec.configure do |config|
+  config.after(:example) do
+    Allure.step(name: 'After hook step')
+    Allure.add_attachment(
+      name: 'After hook attach',
+      source: 'Attachment',
+      type: Allure::ContentType::TXT,
+      test_case: true
+    )
+  end
+end
