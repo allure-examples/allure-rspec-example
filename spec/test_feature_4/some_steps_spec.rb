5b074db92f4be25bdc3b36cf5cb04a4239a7e3c1
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2020-10-30 18:09:15 +0200

    Run only passing spec

---
+++ spec/test_feature_4/some_steps_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative '../spec_helper'
+
+describe 'Some usecase 1', severity: :normal do
+  it 'should have some steps' do |t|
+    t.run_step 'step1' do
+      t.add_attachment(
+        name: 'attachment',
+        source: 'Some string',
+        type: Allure::ContentType::TXT,
+        test_case: false
+      )
+      expect(1).to eq(1)
+    end
+
+    Allure.run_step 'step2' do
+      expect(0).to eq(0)
+    end
+  end
+
+  it 'should pass', issue_1: 'QA-123', issue_2: 'QA-124' do
+    expect(5).to eq(5)
+  end
+end
