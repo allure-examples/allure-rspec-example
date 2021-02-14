22a03ba347a28637d038d89d8a763f586be623d5
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2021-02-14 13:11:05 +0200

    Add nested step

--- spec/test_feature_0/test_case_1_spec.rb
+++ spec/test_feature_0/test_case_1_spec.rb
@@ -5,7 +5,9 @@ require_relative '../spec_helper'
 describe 'test_feature', tms_1: 'QA-123', tms_2: 'QA-124' do
   it 'test_case_1' do |example|
     example.run_step('Pre-Requisites') do
-      expect(20).to be > 19
+      example.run_step('Nested') do
+        expect(20).to be > 19
+      end
     end
 
     example.run_step('Step2') do
